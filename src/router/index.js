a248e2948a4c798779e231c365864396d028cb1a
陈义凯 <805327132@qq.com>
2023-11-03 09:38:50 +0800

    feat: 动态路由开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import views from "./views/index.js";
 import pages from "./page/index.js";
 import website from "@/config/website.js";
+import { menuStore } from "@/store/index.js";
 
 const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
@@ -22,102 +23,85 @@ const router = createRouter({
   },
 });
 
-const viewsModules = import.meta.glob("../views/**/*.vue");
-const pagesModules = import.meta.glob("../pages/**/*.vue");
+const modules = import.meta.glob("../**/**/*.vue");
 
 function isURL(s) {
   if (s.includes("html")) return true;
   return /^http[s]?:\/\/.*/.test(s);
 }
 
-export function addRoute(aMenu, first) {
-  if (aMenu.length === 0) return;
-
-  const propsConfig = website.menu.props;
-  const propsDefault = {
-    label: propsConfig.label || "menuName",
-    path: propsConfig.path || "path",
-    icon: propsConfig.icon || "icon",
-    children: propsConfig.children || "children",
-    meta: propsConfig.meta || "meta",
-  };
-
+export function addRoute(aMenu = [], first) {
+  const aRouter = [];
+  const propsDefault = website.menu.props;
+  if (aMenu && aMenu.length === 0) return;
   for (let i = 0; i < aMenu.length; i++) {
     const oMenu = aMenu[i];
-
-    // 不重复添加
-    if (router.hasRoute(oMenu[propsDefault["path"]])) return;
-    const isChild = oMenu[propsDefault.children].length !== 0;
-    const path = (() => {
-      if (first) {
-        // 将 '/index' 替换为 ''
-        return oMenu[propsDefault.path].replace("/index", "");
-      } else {
-        return oMenu[propsDefault.path];
-      }
-    })();
-    const redirect = (() => {
-      // 第一次进来但是没有子路由的 需要添加redirect
-      if (!isChild && first && !isURL(this.path)) return `${this.path}/index`;
-      else return "";
-    })();
-    const name = oMenu[propsDefault.label];
-    const icon = oMenu[propsDefault.icon];
-    const component = (() => {
-      let component = null;
-      if (first) {
-        component = pagesModules[`../pages/Layout/index.vue`];
-      } else if (isChild && !first) {
-        component = pagesModules[`../pages/Layout/default.vue`];
-      } else {
-        component = viewsModules[`../views${oMenu.path}.vue`];
-      }
-      return component;
-    })();
-    const children = (() => {
-      let _children = oMenu[propsDefault.children];
-      if (!isChild) {
-        if (!first) return [];
-
-        if (!isURL(path)) oMenu[propsDefault.path] = `${path}/index`;
-
-        return [
-          {
-            component,
-            icon: icon,
-            name: name,
-            meta: meta,
-            path: "index",
-          },
-        ];
-      } else {
-        return addRoute(_children, false);
-      }
-    })();
-    let meta = oMenu[propsDefault.meta] || {};
-    meta = Object.assign(
-      meta,
-      (function () {
-        if (oMenu.keepAlive === 0) {
-          return {
-            keepAlive: oMenu.keepAlive,
-            $keepAlive: true,
-          };
+    let path = oMenu[propsDefault.path],
+      component = oMenu.component,
+      name = oMenu[propsDefault.label],
+      icon = oMenu[propsDefault.icon],
+      children = oMenu[propsDefault.children],
+      query = oMenu[propsDefault.query],
+      meta = oMenu[propsDefault.meta];
+    const isChild = !!(children && children.length !== 0);
+    const oRouter = {
+      path: path,
+      component: (() => {
+        // 判断是否为首路由
+        if (first) {
+          return modules["../pages/Layout/index.vue"];
+          // 判断是否为多层路由
+        } else if (isChild && !first) {
+          return modules[`../pages/Layout/index.vue`];
+          // 判断是否为最终的页面视图
+        } else {
+          return modules[`../views${oMenu.path}.vue`];
         }
       })(),
-    );
-
-    const route = {
-      path,
-      redirect,
       name,
       icon,
-      component,
-      children,
       meta,
+      query,
+      redirect: (() => {
+        if (!isChild && first) return `${path}`;
+        else return "";
+      })(),
+      // 处理是否为一级路由
+      children: !isChild
+        ? (() => {
+            if (first) {
+              oMenu[propsDefault.path] = `${path}`;
+              let result = modules[`../views${oMenu.path}.vue`];
+              if (result) result().then((mod) => (mod.default.name = path));
+              else {
+                console.log(component + "不存在");
+              }
+              return [
+                {
+                  component: result,
+                  icon: icon,
+                  name: name,
+                  meta: meta,
+                  query: query,
+                  path: "",
+                },
+              ];
+            }
+            return [];
+          })()
+        : (() => {
+            return addRoute(children, false);
+          })(),
     };
-    router.addRoute("Layout", route);
+    if (!isURL(path)) aRouter.push(oRouter);
+  }
+  if (first) {
+    aRouter.forEach((ele) => router.addRoute(ele));
+  } else {
+    return aRouter;
   }
 }
+
+addRoute(menuStore["menuList"], true);
 
 export default router;
