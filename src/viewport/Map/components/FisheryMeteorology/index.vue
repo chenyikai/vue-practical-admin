0d1ceb514967aafbcecc83fdc96ed73bb4f22f59
Lingluo <15068011381@139.com>
2024-06-17 10:16:12 +0800

    update

---
+++ src/viewport/Map/components/FisheryMeteorology/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { watch } from "vue";
+import { mapStore } from "@/store/index.js";
+
+defineOptions({
+  name: "FisheryMeteorology",
+});
+
+const MapStore = mapStore();
+
+function open() {}
+
+watch(
+  () => MapStore.$state.qxType,
+  (news) => {
+    open(news);
+  },
+);
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped lang="scss"></style>
