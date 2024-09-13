8a8b427911e6ead3b1f204c2bc1d56cdecd16579
陈义凯 <805327132@qq.com>
2024-09-13 09:01:38 +0800

    fix: 默认图片无法加载

--- src/config/website.js
+++ src/config/website.js
@@ -1,12 +1,12 @@
 export default {
   title: import.meta.env.VITE_TITLE, // 网站标题(项目名称)
   storageKey: "group-two-vue3-admin", // 可设置为项目英文名 避免不同项目在同一环境下缓存互相串联
-  indexPage: "/index", // 项目首页地址
-  logo: `${import.meta.env.BASE_URL}vite.svg`, // 项目或企业LOGO
-  defaultTabIcon: `${import.meta.env.BASE_URL}tab.svg`, // 项目或企业LOGO
+  indexPage: "index", // 项目首页地址
+  logo: `${import.meta.env.BASE_URL}/vite.svg`, // 项目或企业LOGO
+  defaultTabIcon: `${import.meta.env.BASE_URL}/tab.svg`, // 项目或企业LOGO
   defaultAvatar: {
-    male: `${import.meta.env.BASE_URL}male.svg`,
-    female: `${import.meta.env.BASE_URL}female.svg`,
+    male: `${import.meta.env.BASE_URL}/male.svg`,
+    female: `${import.meta.env.BASE_URL}/female.svg`,
   },
   upload: {
     url: "/rest/file/upload",
